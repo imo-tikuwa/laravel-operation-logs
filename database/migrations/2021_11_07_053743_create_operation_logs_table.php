b550078b97d681892ab7ac8add6b031e8c5bd662
imo-tikuwa <imotikuwabathclean@gmail.com>
2021-11-07 17:29:06 +0900

    日本語のコメント句削除
    ide-helper:modelsを再実行

--- database/migrations/2021_11_07_053743_create_operation_logs_table.php
+++ database/migrations/2021_11_07_053743_create_operation_logs_table.php
@@ -15,11 +15,11 @@ class CreateOperationLogsTable extends Migration
     {
         Schema::create('operation_logs', function (Blueprint $table) {
             $table->id();
-            $table->string('client_ip')->nullable(false)->comment('クライアントIP');
-            $table->text('user_agent')->comment('ユーザーエージェント');
-            $table->string('request_url', 255)->nullable(false)->comment('リクエストURL');
-            $table->dateTime('request_time', 6)->nullable(false)->comment('リクエスト日時');
-            $table->dateTime('response_time', 6)->nullable(false)->comment('レスポンス日時');
+            $table->string('client_ip')->nullable(false)->comment('client ip');
+            $table->text('user_agent')->comment('user agent');
+            $table->string('request_url', 255)->nullable(false)->comment('request url');
+            $table->dateTime('request_time', 6)->nullable(false)->comment('request time');
+            $table->dateTime('response_time', 6)->nullable(false)->comment('response time');
         });
     }
 
